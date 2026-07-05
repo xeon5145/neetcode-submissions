c0a809f9bd4a7be3084f4f007f8879f674ebc8ed
Abhishek Kagra <62745709+xeon5145@users.noreply.github.com>
2026-07-05 18:54:32 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # check1 = False
+        # check2 = False
+
+        # # Check 1 : Length
+        # if len(s) == len(t):
+        #     check1 = True
+        # else:
+        #     check1 = False
+
+        # # Check 2 : Characters
+        # if sorted(s) == sorted(t):
+        #     check2 = True
+        # else:
+        #     check2 = False
+
+        # if check1 == True and check2 == True:
+        #     return True
+        # else:
+        #     return False
+
+        # Smaller one
+        if (sorted(s) == sorted(t)) and (len(s) == len(t)):
+            return True
+        else:
+            return False
+        
